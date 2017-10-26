a092060474a45618a77d3d4859d74938c36b59a5
AnitaPanwar <anita.panwar0503@gmail.com>
2017-10-26 11:18:47 +0530

    added file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'anita.risedigitech@gmail.com',
+    password:             'risedigitech@014',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root 'home#index'
-  get '/about', to: 'home#about', as: 'about'
-  get '/blog', to: 'home#blog', as: 'blog'
-  get '/contact', to: 'home#contact', as: 'contact'
-  get '/service', to: 'home#service', as: 'service'
-  get '/team', to: 'home#team', as: 'team'
-  get '/portfolio', to: 'home#portfolio', as: 'portfolio'
+  
+get 'home/index'
+root 'home#index'
+get '/about', to: 'home#about', as: 'about'
+get '/blog', to: 'home#blog', as: 'blog'
+ 
+get '/service', to: 'home#service', as: 'service'
+get '/team', to: 'home#team', as: 'team'
+get '/portfolio', to: 'home#portfolio', as: 'portfolio'
+get 'contact', to: 'contact#new', as: 'contact'
+post 'contact', to: 'contact#create_mail'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
